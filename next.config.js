9bca2eb67195ab9a2d349f208108d12a6136d2ec
OthanJara <jonathan.jara1216@gmail.com>
2026-04-04 07:14:47 +0800

    Add base path for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
